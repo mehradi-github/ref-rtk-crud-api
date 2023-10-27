06cf3cc1eb9e43dc70b8cf2545860844025e0748
Mehradi <mehradi.info@gmail.com>
2023-10-27 14:37:11 +0200

    set the CORS policy

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,26 @@ import express, { Express } from "express";
 import routes from "./routes/contact";
 
 const router: Express = express();
+
 router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
 router.use("/", routes);
+
+router.use((req, res, next) => {
+  // set the CORS policy
+  res.header("Access-Control-Allow-Origin", "*");
+  // set the CORS headers
+  res.header(
+    "Access-Control-Allow-Headers",
+    "origin, X-Requested-With,Content-Type,Accept, Authorization"
+  );
+  // set the CORS method headers
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 const httpServer = http.createServer(router);
 
