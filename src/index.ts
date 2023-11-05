8f3cb9b5c2b4e8f1c4155db2a6ff3ea67b7a3119
Mehradi <mehradi.info@gmail.com>
2023-11-05 08:30:45 +0100

    Adding cors

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,41 @@
 import http from "http";
 import express, { Express } from "express";
 import routes from "./routes/contact";
+import cors from "cors";
 
-const router: Express = express();
+const app: Express = express();
 
-router.use(express.json());
-router.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
+// app.use((req, res, next) => {
+//   // set the CORS policy
+//   res.header("Access-Control-Allow-Origin", "*");
+//   // set the CORS headers
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "origin, X-Requested-With,Content-Type,Accept, Authorization"
+//   );
+//   // set the CORS method headers
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
-router.use((req, res, next) => {
-  // set the CORS policy
-  res.header("Access-Control-Allow-Origin", "*");
-  // set the CORS headers
-  res.header(
-    "Access-Control-Allow-Headers",
-    "origin, X-Requested-With,Content-Type,Accept, Authorization"
-  );
-  // set the CORS method headers
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
-    return res.status(200).json({});
-  }
-  next();
-});
+app.use("/", routes);
+const httpServer = http.createServer(app);
 
-router.use("/", routes);
-const httpServer = http.createServer(router);
-
-httpServer.listen(5000, () => {
-  console.log("server is running on port 5000");
+httpServer.listen(5002, () => {
+  console.log("server is running on port 5002");
 });
